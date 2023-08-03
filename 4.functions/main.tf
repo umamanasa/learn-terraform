cd1e67f42e915fafdb1547c0517de1c13d91a6ab
umamanasa <umamanasa49@gmail.com>
2023-08-03 12:47:34 +0530

    functions in terraform

--- 4.functions/main.tf
+++ 4.functions/main.tf
@@ -7,3 +7,13 @@ output "fruits" {
   value = element(var.fruits, 2 )
 }
 
+variable "fruits_with_stock" {
+  default = {
+    apple = 100
+  }
+}
+output "fruits_stock" {
+  value = var.fruits_with_stock["banana"]
+}
+
+
