a2c8ae1702249b0272406897131894895d5991ef
umamanasa <umamanasa49@gmail.com>
2023-08-03 11:23:51 +0530

    terraform variables

--- 3.vars/sample.tf
+++ 3.vars/sample.tf
@@ -1,6 +1,38 @@
+#plain variables
 variable "fruit_name" {
   default = "apple"
 }
 output "fruit_name" {
   value = var.fruit_name
+}
+
+#list variables
+variable "fruits" {
+  default = [
+    "apple",
+    "banana"
+  ]
+  # default = [ "apple", "banana" ]
+}
+
+#map variable, plain
+variable "fruit_stock" {
+  default = {
+    apple = 100
+    banana = 50
+  }
+}
+
+#map variable, map of maps
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 100
+      price = 3
+  }
+    banana = {
+      stock = 50
+      price = 1
+    }
+  }
 }
