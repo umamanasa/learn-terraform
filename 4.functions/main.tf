50b4967d11a1922bfc6ebe6400511577fe061bf1
umamanasa <umamanasa49@gmail.com>
2023-08-03 12:33:22 +0530

    functions in terraform

--- 4.functions/main.tf
+++ 4.functions/main.tf
@@ -1,7 +1,9 @@
+# Functions in Terraform
 variable "fruits" {
   default = [ "apple", "banana" ]
 }
 output "fruits" {
-  value = var.fruits[2]
+#  value = var.fruits[2]
+  value = element(var.fruits, 2 )
 }
 
--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,17 +1,214 @@
-#variable "components" {
-#  default = "frontend", "mongodb", "catalogue", "redis"
-#}
+variable "ami" {
+  default = "ami-03265a0778a880afb"
+}
 
-variable "components" {
-  default = {
-    frontend = {
-    instance_type = "t3.micro"
-    }
-    mongodb = {
-    instance_type = "t3.micro"
-    }
-    frontend = {
-      instance_type = "t3.micro"
-    }
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "security_group_ids" {
+  default = [ "sg-041096a23e28b0eb0" ]
+}
+
+variable "zone_id" {
+  default = "Z0365188L7MG2LV8YN4J"
+}
+
+
+resource "aws_instance" "frontend" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group_ids
+
+  tags = {
+    Name = "frontend"
   }
 }
+
+resource "aws_route53_record" "frontend" {
+  zone_id = var.zone_id
+  name    = "frontend-dev.manasareddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+#resource "aws_instance" "mongodb" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+#
+#  tags = {
+#    Name = "mongodb"
+#  }
+#}
+#
+#resource "aws_route53_record" "mongodb" {
+#  zone_id = "Z0365188L7MG2LV8YN4J"
+#  name    = "mongodb-dev.manasareddy.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.mongodb.private_ip]
+#}
+#
+#resource "aws_instance" "catalogue" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+#
+#  tags = {
+#    Name = "catalogue"
+#  }
+#}
+#
+#resource "aws_route53_record" "catalogue" {
+#  zone_id = "Z0365188L7MG2LV8YN4J"
+#  name    = "catalogue-dev.manasareddy.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.catalogue.private_ip]
+#}
+#
+#resource "aws_instance" "redis" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+#
+#  tags = {
+#    Name = "redis"
+#  }
+#}
+#
+#resource "aws_route53_record" "redis" {
+#  zone_id = "Z0365188L7MG2LV8YN4J"
+#  name    = "redis-dev.manasareddy.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.redis.private_ip]
+#}
+#
+#resource "aws_instance" "user" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+#
+#  tags = {
+#    Name = "user"
+#  }
+#}
+#
+#resource "aws_route53_record" "user" {
+#  zone_id = "Z0365188L7MG2LV8YN4J"
+#  name    = "user-dev.manasareddy.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.user.private_ip]
+#}
+#
+#resource "aws_instance" "cart" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+#
+#  tags = {
+#    Name = "cart"
+#  }
+#}
+#
+#resource "aws_route53_record" "cart" {
+#  zone_id = "Z0365188L7MG2LV8YN4J"
+#  name    = "cart-dev.manasareddy.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.cart.private_ip]
+#}
+#
+#resource "aws_instance" "mysql" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+#
+#  tags = {
+#    Name = "mysql"
+#  }
+#}
+#
+#resource "aws_route53_record" "mysql" {
+#  zone_id = "Z0365188L7MG2LV8YN4J"
+#  name    = "mysql-dev.manasareddy.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.mysql.private_ip]
+#}
+#
+#resource "aws_instance" "shipping" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+#
+#  tags = {
+#    Name = "shipping"
+#  }
+#}
+#
+#resource "aws_route53_record" "shipping" {
+#  zone_id = "Z0365188L7MG2LV8YN4J"
+#  name    = "shipping-dev.manasareddy.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.shipping.private_ip]
+#}
+#
+#resource "aws_instance" "rabbitmq" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+#
+#  tags = {
+#    Name = "rabbitmq"
+#  }
+#}
+#
+#resource "aws_route53_record" "rabbitmq" {
+#  zone_id = "Z0365188L7MG2LV8YN4J"
+#  name    = "rabbitmq-dev.manasareddy.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.rabbitmq.private_ip]
+#}
+#
+#resource "aws_instance" "payment" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+#
+#  tags = {
+#    Name = "payment"
+#  }
+#}
+#
+#resource "aws_route53_record" "payment" {
+#  zone_id = "Z0365188L7MG2LV8YN4J"
+#  name    = "payment-dev.manasareddy.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.payment.private_ip]
+#}
+#
+#resource "aws_instance" "dispatch" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+#
+#  tags = {
+#    Name = "dispatch"
+#  }
+#}
+#
+#resource "aws_route53_record" "dispatch" {
+#  zone_id = "Z0365188L7MG2LV8YN4J"
+#  name    = "dispatch-dev.manasareddy.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.dispatch.private_ip]
+#}
