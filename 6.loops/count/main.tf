f12695f5c622e98f135145397beccab134b45789
umamanasa <umamanasa49@gmail.com>
2023-08-04 11:58:46 +0530

    terraform loops

--- 6.loops/count/main.tf
+++ 6.loops/count/main.tf
@@ -11,6 +11,6 @@ resource "aws_instance" "instance" {
 
   tags = {
 #   Name = var.components[count.index]
-    Name = element(var.components,count.index)
+    Name = element(var.components, count.index)
   }
 }
