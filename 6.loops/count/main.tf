787acf99a60917d9222d9b5701a43fb0fcdec06c
umamanasa <umamanasa49@gmail.com>
2023-08-04 11:45:41 +0530

    terraform loops

---
+++ 6.loops/count/main.tf
@@ -0,0 +1,14 @@
+variable "components" {
+  default = [ "frontend", "mongodb" ]
+}
+resource "aws_instance" "instance" {
+  count = length(var.components)
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-041096a23e28b0eb0" ]
+
+  tags = {
+    //Name = var.components[count.index]
+    name = elements(var.components,count.index)
+  }
+}
