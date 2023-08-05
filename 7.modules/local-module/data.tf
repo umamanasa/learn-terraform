3e7a987f42c0b2dbf53b636f57132cdfda391b55
umamanasa <umamanasa49@gmail.com>
2023-08-05 19:38:28 +0530

    terraform modules

--- 7.modules/local-module/data.tf
+++ 7.modules/local-module/data.tf
@@ -7,3 +7,8 @@ output "aws_ami" {
   value = data.aws_ami.ami
 }
 
+resource "aws_instance" "instance" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-041096a23e28b0eb0"]
+}
