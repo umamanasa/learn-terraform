339c8066004fb52ee44c8fff843a4a17d1e432f8
umamanasa <umamanasa49@gmail.com>
2024-03-21 15:45:28 +0530

    terraform ex

--- example/example.tf
+++ example/example.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "web" {
   ami           = "ami-0f3c7d07486cad139"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "frontend"
   }
 }
 
