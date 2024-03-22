4b872bc2586f1587e1b1888825808f6a7c1f3cb6
umamanasa <umamanasa49@gmail.com>
2024-03-22 17:25:05 +0530

    creating state files

--- expense-v3/module/main.tf
+++ expense-v3/module/main.tf
@@ -18,18 +18,18 @@ resource "aws_route53_record" "record" {
   records = [ aws_instance.instance.private_ip ]
 }
 
-#resource "null_resource" "ansible" {
-#
-#  depends_on = [
-#    aws_route53_record.record
-#  ]
-#
-#  provisioner "local-exec" {
-#    command = <<EOF
-#cd /home/centos/expense-ansible
-#git pull
-#sleep 30
-#ansible-playbook -i ${var.name}-dev.manasareddy.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
-#EOF
-#  }
-#}
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/expense-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.manasareddy.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
+}
