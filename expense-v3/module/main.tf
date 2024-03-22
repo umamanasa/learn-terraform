9b35133e1345bd67d451a4088ccdb1b6963d5fbb
umamanasa <umamanasa49@gmail.com>
2024-03-22 15:20:27 +0530

    using modules

--- expense-v3/module/main.tf
+++ expense-v3/module/main.tf
@@ -12,24 +12,24 @@ resource "aws_instance" "instance" {
 resource "aws_route53_record" "record" {
 
   zone_id = var.zone_id
-  name    = "${var.name}.manasareddy.online"
+  name    = "${var.name}-dev.manasareddy.online"
   type    = "A"
   ttl     = 30
   records = [ aws_instance.instance.private_ip ]
 }
 
-resource "null_resource" "ansible" {
-
-  depends_on = [
-    aws_route53_record.record
-  ]
-
-  provisioner "local-exec" {
-    command = <<EOF
-cd /home/centos/expense-ansible
-git pull
-sleep 30
-ansible-playbook -i ${var.name}-dev.manasareddy.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
-EOF
-  }
-}
+#resource "null_resource" "ansible" {
+#
+#  depends_on = [
+#    aws_route53_record.record
+#  ]
+#
+#  provisioner "local-exec" {
+#    command = <<EOF
+#cd /home/centos/expense-ansible
+#git pull
+#sleep 30
+#ansible-playbook -i ${var.name}-dev.manasareddy.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+#EOF
+#  }
+#}
