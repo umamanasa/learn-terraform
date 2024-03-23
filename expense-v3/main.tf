7e7d5c39ba0c73a88dba9d36c83499a3721123f6
umamanasa <umamanasa49@gmail.com>
2024-03-23 11:28:36 +0530

    creating modules in expense

--- expense-v3/main.tf
+++ expense-v3/main.tf
@@ -1,8 +1,8 @@
 module "components" {
 
   for_each = var.components
+  source = "./module"
 
-  source              = "./module"
   zone_id             = var.zone_id
   security_groups     = var.security_groups
   name                = each.value["name"]
