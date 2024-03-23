974f09a57ba546ee0b996fa8803100c2b8179621
umamanasa <umamanasa49@gmail.com>
2024-03-23 11:39:38 +0530

    creating modules in expense

--- expense-v3/main.tf
+++ expense-v3/main.tf
@@ -1,11 +1,11 @@
 module "components" {
 
   for_each = var.components
-  source = "./module"
 
-  zone_id             = var.zone_id
-  security_groups     = var.security_groups
-  name                = each.value["name"]
-  instance_type       = each.value["instance_type"]
-  }
+  source            = "./module"
+  zone_id           = var.zone_id
+  security_groups   = var.security_groups
+  name              = each.value["name"]
+  instance_type     = each.value["instance_type"]
 
+}
--- expense-v3/module/main.tf
+++ expense-v3/module/main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "instance" {
-  ami                     = data.aws_ami.ami
-  instance_type           = var.instance_type
-  vpc_security_group_ids  = var.security_groups
+
+  ami                       = data.aws_ami.ami.id
+  instance_type             = var.instance_type
+  vpc_security_group_ids    = var.security_groups
 
   tags = {
     Name = var.name
@@ -9,12 +10,11 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_route53_record" "record" {
-
-  zone_id = var.zone_id
-  name    = "${var.name}-dev.manasareddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [ aws_instance.instance.private_ip ]
+  zone_id     = var.zone_id
+  name        = "${var.name}-dev.manasareddy.online"
+  type        = "A"
+  ttl         = 30
+  records     = [ aws_instance.instance.private_ip ]
 }
 
 #resource "null_resource" "ansible" {
